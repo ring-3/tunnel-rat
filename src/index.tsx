b339a64cc7da7802c835fac34af648eea0ec8090
James Vaughan <james@jamesbvaughan.com>
2025-05-15 07:24:12 -0700

    Refactor the new state management system

--- src/index.tsx
+++ src/index.tsx
@@ -3,70 +3,65 @@ import { useIsomorphicLayoutEffect } from './utils'
 
 type Props = { children: React.ReactNode }
 
-export default function tunnel() {
-  let rats: Array<React.ReactNode> = []
-  const ratNotifiers: Array<() => void> = []
-  function subscribeToRatChanges(onRatsChange: () => void) {
-    ratNotifiers.push(onRatsChange)
+function createStore<TState>(initialState: TState) {
+  let state: TState = initialState
+  const notifiers: Array<() => void> = []
+
+  function subscribe(onChange: () => void) {
+    notifiers.push(onChange)
     return () => {
-      ratNotifiers.splice(ratNotifiers.indexOf(onRatsChange), 1)
-    }
-  }
-  function getRats() {
-    return rats
-  }
-  function setRats(newRats: Array<React.ReactNode>) {
-    rats = newRats
-    for (const notify of ratNotifiers) {
-      notify()
+      notifiers.splice(notifiers.indexOf(onChange), 1)
     }
   }
 
-  let version = 0
-  const versionNotifiers: Array<() => void> = []
-  function subscribeToVersionChanges(onVersionChange: () => void) {
-    versionNotifiers.push(onVersionChange)
-    return () => {
-      versionNotifiers.splice(versionNotifiers.indexOf(onVersionChange), 1)
-    }
-  }
-  function getVersion() {
-    return version
-  }
-  function setVersion(newVersion: number) {
-    version = newVersion
-    for (const notify of versionNotifiers) {
-      notify()
-    }
+  function getState() {
+    return state
   }
 
   return {
+    useState() {
+      return useSyncExternalStore(subscribe, getState)
+    },
+    setState(updateState: (currentState: TState) => TState) {
+      state = updateState(state)
+      for (const notify of notifiers) {
+        notify()
+      }
+    },
+  }
+}
+
+export default function tunnel() {
+  const ratsStore = createStore<Array<React.ReactNode>>([])
+  const versionStore = createStore(0)
+
+  return {
     In: ({ children }: Props) => {
-      const currentVersion = useSyncExternalStore(subscribeToVersionChanges, getVersion)
+      const version = versionStore.useState()
 
       /* When this component mounts, we increase the version number.
       This will cause all existing rats to re-render (just like if the Out component
       were mapping items to a list.) The re-rendering will cause the final
       order of rendered components to match what the user is expecting. */
       useIsomorphicLayoutEffect(() => {
-        setVersion(version + 1)
+        versionStore.setState((currentVersion) => currentVersion + 1)
       }, [])
 
       /* Any time the children _or_ the version number change, insert
       the specified React children into the list of rats. */
       useIsomorphicLayoutEffect(() => {
-        setRats([...rats, children])
+        ratsStore.setState((rats) => [...rats, children])
         return () => {
-          setRats(rats.filter((child) => child !== children))
+          ratsStore.setState((rats) => rats.filter((child) => child !== children))
         }
-      }, [children, currentVersion])
+      }, [children, version])
 
       return null
     },
 
     Out: () => {
-      const children = useSyncExternalStore(subscribeToRatChanges, getRats)
-      return <>{children}</>
+      const rats = ratsStore.useState()
+      return <>{rats}</>
     },
   }
 }
